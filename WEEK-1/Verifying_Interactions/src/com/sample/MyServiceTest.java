dd5f614d7a8fe5da04c858d3354556a12d58f67c
Alekhya1246 <alekhyayadavveerla1215@gmail.com>
2026-06-25 14:42:40 +0530

    Updated project files

---
+++ WEEK-1/Verifying_Interactions/src/com/sample/MyServiceTest.java
@@ -0,0 +1,25 @@
+package com.sample;
+
+import static org.mockito.Mockito.verify;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+public class MyServiceTest {
+
+    @Test 
+    public void testVerifyInteraction() {
+
+        // Create Mock Object
+        ExternalApi mockApi = Mockito.mock(ExternalApi.class);
+
+        // Create Service
+        MyService service = new MyService(mockApi);
+
+        // Call Method
+        service.fetchData();
+
+        // Verify Interaction
+        verify(mockApi).getData();
+    }
+}
