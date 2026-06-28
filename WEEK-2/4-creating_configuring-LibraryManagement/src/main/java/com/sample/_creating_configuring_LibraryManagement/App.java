f7a7440cc61dbb404a7d405c8c5c946b95883f60
Alekhya1246 <alekhyayadavveerla1215@gmail.com>
2026-06-28 11:34:35 +0530

    Updated project files

---
+++ WEEK-2/4-creating_configuring-LibraryManagement/src/main/java/com/sample/_creating_configuring_LibraryManagement/App.java
@@ -0,0 +1,13 @@
+package com.sample._creating_configuring_LibraryManagement;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
